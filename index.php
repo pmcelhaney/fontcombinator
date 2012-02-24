ab63c561c12f72def7ff65e9c5febb94a347c70f
Chip Cullen <chip@chipcullen.com>
2012-02-23 20:43:43 -0500

    Wow - F YOU Git!

--- index.php
+++ index.php
@@ -14,125 +14,158 @@ ini_set('display_errors', 'on');
 
 	<title>The Web Font Combinator</title>
 	
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+	<script>
+	if (typeof jQuery == 'undefined') {
+	document.write('<script src="', 'javascript/jquery-1.7.1.min.js', '" type="text/JavaScript"><\/script>');
+	}
+	</script>
+	<script src="javascript/chosen/chosen.jquery.min.js" type="text/javascript" charset="utf-8"></script>
+	<script src="javascript/colorpicker.js" type="text/javascript" charset="utf-8"></script>
+	
+	<script src="javascript/fc_functions.js" type="text/javascript" charset="utf-8"></script>
+	
+	<link rel="stylesheet" href="javascript/chosen/chosen.css" type="text/css" media="screen" title="no title" charset="utf-8" />
+	<link rel="stylesheet" href="css/fc_style.css" type="text/css" media="screen" title="no title" charset="utf-8" />
+	<link rel="stylesheet" href="css/colorpicker.css" type="text/css" media="screen" title="no title" charset="utf-8" />
+	
 	<style type="text/css" media="screen">
+	* {
+		margin: 0;
+		padding: 0;
+		font-weight: regular;
+	}
+	
 	h1 {
 		font-size: 30px;
+		font-size: regular;
+		<?php
+		if(isset($_GET['h1'])){ 
+		   	 echo 'font-family: ' . $_GET['h1'] . ';';	 
+		    }
+		if(isset($_GET['h1size'])){ 
+			   	 echo 'font-size: ' . $_GET['h1size'] . 'px;';	 
+			    }
+			
+		// if(isset($_GET['h1w'])){ 
+		// 	   	 echo 'font-weight: ' . $_GET['h1w'] . ';';	 
+		// 	    }
+		// if(isset($_GET['h1s'])){ 
+		// 	   	 echo 'font-style: ' . $_GET['h1s'] . ';';	 
+		// 	    }
+	
+		if(isset($_GET['h1color'])){ 
+			   	 echo 'color: #' . $_GET['h1color'] . ';';	 
+			    }
+		?>
 	}
 	
 	h2 {
 		font-size: 20px;
+		<?php
+		if(isset($_GET['h2'])){ 
+		   	 echo 'font-family: ' . $_GET['h2'] . ';';	 
+		    }
+		if(isset($_GET['h2size'])){ 
+	   	 echo 'font-size: ' . $_GET['h2size'] . 'px;';	 
+	    }
+	
+		if(isset($_GET['h2color'])){ 
+				echo 'color: #' . $_GET['h2color'] . ';';	 
+			}
+		?>
 	}
 	
 	p {
 		font-size: 16px;
 		line-height: 1.4;
-	}
-	
-    <?php 
-	if(isset($_GET['h1'])){ 
-	   	 echo 'h1 { font-family: ' . $_GET['h1'] . ';}';	 
-	    }
-	if(isset($_GET['h1size'])){ 
-		   	 echo 'h1 { font-size: ' . $_GET['h1size'] . 'px;}';	 
-		    }
-	
-	if(isset($_GET['h1color'])){ 
-			   	 echo 'h1 { color: #' . $_GET['h1color'] . ';}';	 
-			    }
-	
-	
-	if(isset($_GET['h2'])){ 
-	   	 echo 'h2 { font-family: ' . $_GET['h2'] . ';}';	 
-	    }
-	if(isset($_GET['h2size'])){ 
-   	 echo 'h2 { font-size: ' . $_GET['h2size'] . 'px;}';	 
-    }
-	
-	if(isset($_GET['h1color'])){ 
-			echo 'h2 { color: #' . $_GET['h2color'] . ';}';	 
-		}
-	
-	
-	
+		<?php
 		if(isset($_GET['p'])){ 
-		   	 echo 'p { font-family: ' . $_GET['p'] . ';}';	 
+		   	 echo 'font-family: ' . $_GET['p'] . ';';	 
 		    }
 		if(isset($_GET['h2size'])){ 
-	   	 echo 'p { font-size: ' . $_GET['psize'] . 'px;}';	 
+	   	 echo 'font-size: ' . $_GET['psize'] . 'px;';	 
 	    }
 	
 		if(isset($_GET['pcolor'])){ 
-				echo 'p { color: #' . $_GET['pcolor'] . ';}';	 
+				echo 'color: #' . $_GET['pcolor'] . ';';	 
 			}
 
-			if(isset($_GET['plh'])){ 
-					echo 'p { line-height: ' . $_GET['plh'] . ';}';	 
-				}
+		if(isset($_GET['plh'])){ 
+				echo 'line-height: ' . $_GET['plh'] . ';';	 
+			}
+		?>
+	}
 	
-	
-	?>
-
-	</style>
-	
-	
+	select.variant_select {
+		text-transform: capitalize;
+	}
+	</style>	
 </head>
-
 <body>
 	
-
-		
+	<section class="content_main">
+		<h1 id="h1_text" contenteditable="true">The Web Font Combinator</h1>
+		<h2 id="h2_text" contenteditable="true">A Web Typography Tool</h2>
+		<div id="p_text" contenteditable="true">
+			<p>Donec ante. afafSed at velit. Vestibulum at purus at urna porttitor sodales. Nullam pulvinar, urna interdum eleifend sodales, eros est tempus quam, quis ultricies nibh elit vitae urna. Donec pretium arcu at quam. Quisque tristique, lacus id tempor blandit, quam massa imperdiet lorem, porta fermentum quam ante ac tortor. Curabitur mauris lectus, dapibus ut, ornare sit amet, vulputate sit amet, erat.</p>
+			<p>Donec ante. Sed at velit. Vestibulum at purus at urna porttitor sodales. Nullam pulvinar, urna interdum eleifend sodales, eros est tempus quam, quis ultricies nibh elit vitae urna. Donec pretium arcu at quam. Quisque tristique, lacus id tempor blandit, quam massa imperdiet lorem, porta fermentum quam ante ac tortor. Curabitur mauris lectus, dapibus ut, ornare sit amet, vulputate sit amet, erat.</p>
+		</div>
+	</section>
 	
-	<form action="index.php" method="get" accept-charset="utf-8">
+	<form action="index.php" method="get" accept-charset="utf-8" id="controls">
 
-
-		<section id="h1_sec">
-			<h1 id="the_web_font_combinator">The Web Font Combinator</h1>
+		<select name="control_option" id="control_option">
+			<option value="h1">Headline (H1)</option>
+			<option value="h2">Subhead (H2)</option>
+			<option value="p">Body text (p)</option>
+			<option value="bg">Background</option>
+		</select>
+		<section id="h1_sec" class="control active">
+			
 			<select name="h1" id="h1_select">
 				<?php 
 					include 'default_font_list_h1.php';
 				?>	
 			</select>
 			<label for="h1size">Size:</label>
-			<input type="number" name="h1size" value="<?php if(isset($_GET['h1size'])) {echo $_GET['h1size']; } else { echo '30'; } ?>"  id="h1size" />px
-			<label for="h1w">Weight:</label>
-			<select name="h1w" id="h1w">
-				<option value="300">300</option>
-				<option value="regular">Regular</option>
-				<option value="bold">Bold</option>
-			</select>
-
-
+			<input type="range" name="h1size" value="<?php if(isset($_GET['h1size'])) {echo $_GET['h1size']; } else { echo '30'; } ?>"  id="h1size" min="0" max="150" />px
+			<label for="h1lh">Line Height</label><input type="range" name="h1lh" value="<?php if(isset($_GET['h1lh'])) {echo $_GET['h1lh']; }  else { echo '1.4'; } ?>" id="h1lh" step="0.01" min="0" max="5"/>
 			<label for="h1color">Color:</label>
 			<input type="text" name="h1color" value="<?php if(isset($_GET['h1color'])) {echo $_GET['h1color']; } ?>" placeholder="000000" id="h1color" />
 			
 		</section>
-		<section id="h2_sec">	
-			<h2 id="h2_text" contenteditable="true">A Web Typography Tool</h2>
+		<section id="h2_sec" class="control">	
+			
 			<select name="h2" id="h2_select">
 				<?php include 'default_font_list_h2.php' ?>	
 			</select>
 			<label for="h2size">Size:</label>
-			<input type="number" name="h2size" value="<?php if(isset($_GET['h2size'])) {echo $_GET['h2size']; } else { echo '20'; } ?>"  id="h2size" />px
+			<input type="range" name="h2size" value="<?php if(isset($_GET['h2size'])) {echo $_GET['h2size']; } else { echo '20'; } ?>"  id="h2size" />px
+			<label for="h2lh">Line Height</label><input type="range" name="plh" value="<?php if(isset($_GET['h2lh'])) {echo $_GET['h2lh']; }  else { echo '1.4'; } ?>" id="h2lh" step="0.01" />
 			<label for="h2color">Color:</label>
 			<input type="text" name="h2color" value="<?php if(isset($_GET['h2color'])) {echo $_GET['h2color']; } ?>" placeholder="000000" id="h2color" />
 			
 		</section>	
-		<section id="p_sec">
-			<p>Donec ante. Sed at velit. Vestibulum at purus at urna porttitor sodales. Nullam pulvinar, urna interdum eleifend sodales, eros est tempus quam, quis ultricies nibh elit vitae urna. Donec pretium arcu at quam. Quisque tristique, lacus id tempor blandit, quam massa imperdiet lorem, porta fermentum quam ante ac tortor. Curabitur mauris lectus, dapibus ut, ornare sit amet, vulputate sit amet, erat.</p>
-			
+		<section id="p_sec" class="control">
 			<select name="p" id="p_select">
 				<?php include 'default_font_list_p.php' ?>	
 			</select>
 			<label for="psize">Size:</label>
-			<input type="number" name="psize" value="<?php if(isset($_GET['psize'])) {echo $_GET['psize']; }  else { echo '16'; } ?>" placeholder="16" id="psize" />px
+			<input type="range" name="psize" value="<?php if(isset($_GET['psize'])) {echo $_GET['psize']; }  else { echo '16'; } ?>" min="5" max="45" id="psize" />px
+			<label for="plh">Line Height</label><input type="range" name="plh" value="<?php if(isset($_GET['plh'])) {echo $_GET['plh']; }  else { echo '1.4'; } ?>" id="plh" step="0.01" />
 			<label for="pcolor">Color:</label>
 			<input type="text" name="pcolor" value="<?php if(isset($_GET['pcolor'])) {echo $_GET['pcolor']; } ?>" placeholder="000000" id="pcolor" />
-			<label for="plh">Line Height</label><input type="number" name="plh" value="<?php if(isset($_GET['plh'])) {echo $_GET['plh']; }  else { echo '1.4'; } ?>" id="plh" step="0.01" />
-			
-			
 		</section>
-		<input type="submit" name="Submit" value="Submit" id="Submit" />
+		<section id="bg_sec" class="control">
+			
+		</section>	
+		
+		<input type="submit"  id="submit" />
 			
 	</form>
+	
+	
+	
 </body>
 </html>
---
+++ javascript/fc_functions.js
@@ -0,0 +1,189 @@
+$(document).ready(function() {
+	var targets = '#h1_select, #h2_select, #p_select';  //these are used throughout
+	
+	// font calls and option set up
+	function getFonts(fontList){
+		var base = "http://fonts.googleapis.com/css?family=";
+		var defaultList = $('#h1_select').children();
+		$(targets).empty();
+		$(targets).append('<option disabled>*** Google Fonts ***</option>');
+		for (var i=0; i < fontList.length; i++) {
+			//this tool is for latin fonts only so far - sorry, Cyrillic and Greek
+			if(fontList[i].subsets.indexOf('latin') !== -1){
+				var fontName = fontList[i].family;
+				var fontCallName = fontList[i].family.replace(/\s+/g, '+');
+				var fontNameLetters = fontList[i].family.replace(/\s+/g, '');
+				//this adds the stylesheet link to Google Web Fonts, but with only the font's name as a subset of characters, for performance
+				$('<link rel="stylesheet" href="' + base + fontCallName +'&subset=latin&text=' + fontNameLetters +'"  type="text/css" />').appendTo('head');
+				$(targets).append('<option value="'+ fontName +'">'+ fontName +'</option>');
+				
+			}
+		}
+		
+		//readding the default font list so it appears at the end of the <select> and everything is ordered properly
+		$(targets).append('<option disabled>*** System Fonts ***</option>');
+		$(targets).append(defaultList);
+		$(targets).chosen();
+		$('li.active-result').each(function(){
+			$(this).css('font-family', $(this).html());
+		});
+		
+	}
+	
+	//error message
+	function noLove(){
+		$('<h2>I&rsquo;m sorry, but we can&rsquo;t seem to reach Google Fonts.</h2>').prependTo('body');
+	}
+	
+	// ajax success function
+	function onJson(data){
+		if(data.kind === "webfonts#webfontList"){
+			getFonts(data.items);
+			changeFonts(data.items);
+		} else {
+			noLove();
+		}
+	}
+	
+
+		
+	
+	// ajax call
+	$.ajax({
+		url: 'https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyAc3a2WfPaSbA1B25u78zFQRfAide8T34c&sort=alpha&sort=desc',
+		dataType: 'jsonp',
+		success: onJson,
+		error: noLove
+	});
+	
+
+	
+
+
+	
+	// hiding submit button when JS is present
+	$('#submit').hide();
+	
+	function changeFonts(fontList){
+		$(targets).change(function(){
+			var base = "http://fonts.googleapis.com/css?family=";
+			var fontName = $(this).val();
+			
+			//handy way to target the right element based on which select changed
+			var elem = $(this).attr('id').split('_select')[0];
+			$(elem).css('font-style', 'normal');
+			//gets rid of any variant drop down added previously
+			$(this).siblings('.variant_select').remove();
+			$('#'+ elem + '_variant_chzn').remove();
+			
+			
+			//this for loop adding variants based on which font is selected
+			for (var i=0; i < fontList.length; i++) {
+				var variants = fontList[i].variants;
+				//checking to see if the selected font has more than one variant
+				if(fontList[i].family === fontName && variants.length > 1){
+					var variantCall = variants.toString();
+					
+					//adding stylesheet call with all variants
+					$('<link rel="stylesheet" href="' + base + fontName.replace(/\s+/g, '+') +':' + variantCall + '&subset=latin" type="text/css" />').appendTo('head');
+					//create a drop down menu
+					$('<select class="variant_select" id="'+ elem +'_variant" name="'+ elem +'v"><select>').insertAfter('#' +elem + '_select_chzn');
+					for (var i=0; i < variants.length; i++) {
+						var variantName = variants[i].replace('italic',' italic');
+						//special handling of variants results
+						if(variantName === 'regular'){
+							$('<option value="400" selected>400</option>').appendTo('#' + elem +'_variant');
+						} else if (variantName === '400'){
+							$('<option value="'+variantName+'" selected>'+variantName+'</option>').appendTo('#' + elem +'_variant');
+						} else if (variantName === ' italic'){
+							$('<option value="400 italic">400 italic</option>').appendTo('#' + elem +'_variant');
+						} else {
+							$('<option value="'+variantName+'">'+variantName+'</option>').appendTo('#' + elem +'_variant');
+						}
+						
+					}
+
+					$(elem).css('font-family', fontName).css('font-weight', '400');
+					//get the chosen drop down to take on the css style
+					$("#" + elem + "_select_chzn .chzn-single").css('font-family', fontName);
+					$('.variant_select').chosen();
+	
+					$('#' + elem + '_variant_chzn').css('font-family', fontName);
+					$('#' + elem + '_variant_chzn li').each(function(){
+						$(this).css('font-family', fontName);
+						if($(this).html().indexOf('italic') === -1){
+							$(this).css('font-weight', $(this).html());
+						} else {
+							$(this).css('font-weight', $(this).html().split(' italic')[0]).css('font-style','italic');
+						}
+					});
+						
+					return false;  //had to throw this in to stop infinite loop
+				} else if(fontList[i].family === fontName){
+					//do something else when there is only one variant
+					//adding plain stylesheet call w/ no variant addendum
+					$('<link rel="stylesheet" href="' + base + fontName.replace(/\s+/g, '+') +'&subset=latin" type="text/css"  />').appendTo('head');
+					//get the chosen drop down to take on the css style
+					$("#" + elem + "_select_chzn .chzn-single").css('font-family', fontName);
+				}
+			} //end of variant for loop
+			
+			//actually, you know, change fonts
+			$(elem).css('font-family', fontName);
+
+		});// end of targets change
+	}  //end of function changeFonts
+	
+	
+	function variantChange(){
+		$('body').on('change','.variant_select',function(){
+			var elem = $(this).attr('id').split('_variant')[0];
+			var variant = $(this).val();
+			if(variant.indexOf(' italic') === -1){
+				$(elem).css('font-weight',variant);
+				$(elem, '#' + elem + '_variant_chzn').css('font-style','normal');
+				$('#' + elem + '_variant_chzn').css('font-weight', variant).css('font-style', 'normal');
+			} else {
+				$(elem).css('font-weight', variant.replace(' italic',''));
+				$(elem).css('font-style','italic');
+				$('#' + elem + '_variant_chzn').css('font-weight', variant).css('font-style', 'italic');
+			}
+			
+		}); //end of variant_select change
+		
+	}
+	variantChange();
+	
+	
+	$('#h1color, #h2color, #pcolor').on('focus', function(){
+		
+		$('#h1color, #h2color, #pcolor').ColorPicker({
+		onShow: function(){
+			thisEl = $(this).attr('id');
+		},
+		onChange: function(hsb, hex, rgb, el) {
+			$('#' + thisEl).val(hex);
+			$(el).ColorPickerHide();
+		},
+		onBeforeShow: function () {
+			$(this).ColorPickerSetColor(this.value);
+		}
+	})
+	.bind('keyup', function(){
+		$(this).ColorPickerSetColor(this.value);
+	});
+});
+
+	$('body').attr('spellcheck',false); //because of firefox's spellcheck, which has a nasty red underline
+	
+
+});
+
+// TODO:
+
+// - add footer/explanation
+// - STYLE - nice design this time, k?
+// - add background color switching
+// - add background texture additions
+// - add bookmarkable string
+	
